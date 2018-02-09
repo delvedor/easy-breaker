5e47b1d707a364f7c5551e472bb4d4ef34cc2ed9
delvedor <tommydelved@gmail.com>
2018-02-09 16:04:43 +0100

    Updated test

--- test.js
+++ test.js
@@ -244,6 +244,161 @@ test('If the circuit is half open should run just one functions', t => {
   }
 })
 
+test('Should support promises', t => {
+  t.plan(1)
+
+  const easyBreaker = EasyBreaker(asyncOpPromise, {
+    threshold: 2,
+    timeout: 1000,
+    resetTimeout: 1000
+  })
+
+  easyBreaker.runp(false)
+    .then(() => t.is(easyBreaker._failures, 0))
+    .catch(err => t.fail(err))
+})
+
+test('Should support promises (errored)', t => {
+  t.plan(2)
+
+  const easyBreaker = EasyBreaker(asyncOpPromise, {
+    threshold: 2,
+    timeout: 1000,
+    resetTimeout: 1000
+  })
+
+  easyBreaker.runp(true)
+    .then(() => t.fail('Should fail'))
+    .catch(err => {
+      t.is(err.message, 'kaboom')
+      t.is(easyBreaker._failures, 1)
+    })
+})
+
+test('Should support promises (error threshold)', t => {
+  t.plan(6)
+
+  const easyBreaker = EasyBreaker(asyncOpPromise, {
+    threshold: 2,
+    timeout: 1000,
+    resetTimeout: 1000
+  })
+
+  easyBreaker.runp(true)
+    .then(() => t.fail('Should fail'))
+    .catch(err => {
+      t.is(err.message, 'kaboom')
+      t.is(easyBreaker._failures, 1)
+
+      easyBreaker.runp(true)
+        .then(() => t.fail('Should fail'))
+        .catch(err => {
+          t.is(err.message, 'kaboom')
+          t.is(easyBreaker._failures, 2)
+
+          easyBreaker.runp(true)
+            .then(() => t.fail('Should fail'))
+            .catch(err => {
+              t.is(err.message, 'Circuit open')
+              t.is(easyBreaker._failures, 2)
+            })
+        })
+    })
+})
+
+test('Should support promises (error timeout)', t => {
+  t.plan(2)
+
+  const easyBreaker = EasyBreaker(asyncOpPromise, {
+    threshold: 2,
+    timeout: 200,
+    resetTimeout: 1000
+  })
+
+  easyBreaker.runp(true, 1000)
+    .then(() => t.fail('Should fail'))
+    .catch(err => {
+      t.is(err.message, 'Timeout')
+      t.is(easyBreaker._failures, 1)
+    })
+})
+
+test('Should support promises (multiple error timeout - threshold)', t => {
+  t.plan(6)
+
+  const easyBreaker = EasyBreaker(asyncOpPromise, {
+    threshold: 2,
+    timeout: 200,
+    resetTimeout: 1000
+  })
+
+  easyBreaker.runp(true, 1000)
+    .then(() => t.fail('Should fail'))
+    .catch(err => {
+      t.is(err.message, 'Timeout')
+      t.is(easyBreaker._failures, 1)
+
+      easyBreaker.runp(true, 1000)
+        .then(() => t.fail('Should fail'))
+        .catch(err => {
+          t.is(err.message, 'Timeout')
+          t.is(easyBreaker._failures, 2)
+
+          easyBreaker.runp(true, 1000)
+            .then(() => t.fail('Should fail'))
+            .catch(err => {
+              t.is(err.message, 'Circuit open')
+              t.is(easyBreaker._failures, 2)
+            })
+        })
+    })
+})
+
+test('If the circuit is half open should run just one functions (with promises)', t => {
+  t.plan(12)
+
+  const easyBreaker = EasyBreaker(asyncOpPromise, {
+    threshold: 2,
+    timeout: 200,
+    resetTimeout: 200
+  })
+
+  easyBreaker.runp(true)
+    .then(() => t.fail('Should fail'))
+    .catch(err => {
+      t.is(err.message, 'kaboom')
+      t.is(easyBreaker._failures, 1)
+
+      easyBreaker.runp(true)
+        .then(() => t.fail('Should fail'))
+        .catch(err => {
+          t.is(err.message, 'kaboom')
+          t.is(easyBreaker._failures, 2)
+          t.is(easyBreaker.state, 'open')
+          setTimeout(again, 300)
+        })
+    })
+
+  function again () {
+    t.is(easyBreaker.state, 'half-open')
+    easyBreaker.runp(true)
+      .then(() => t.fail('Should fail'))
+      .catch(err => {
+        t.is(err.message, 'kaboom')
+        t.is(easyBreaker._failures, 2)
+        t.is(easyBreaker.state, 'open')
+      })
+
+    easyBreaker.runp(false)
+      .then(() => t.fail('Should fail'))
+      .catch(err => {
+        t.is(err.message, 'Circuit open')
+        t.is(easyBreaker._failures, 2)
+        t.is(easyBreaker.state, 'open')
+      })
+  }
+})
+
 function asyncOp (shouldError, delay, callback) {
   if (callback == null) {
     callback = delay
@@ -253,4 +408,13 @@ function asyncOp (shouldError, delay, callback) {
   setTimeout(() => {
     callback(shouldError ? new Error('kaboom') : null)
   }, delay)
+}
+
+function asyncOpPromise (shouldError, delay) {
+  delay = delay || 0
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      shouldError ? reject(new Error('kaboom')) : resolve(null)
+    }, delay)
+  })
 }
